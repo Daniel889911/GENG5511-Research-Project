60ab5736580bfc124f50491b6202942a909d0fba
danielsantosa <55735053+danielsantosa@users.noreply.github.com>
2022-10-07 21:34:12 +0800

    Added methods to Group_Metrics

--- annotator.py
+++ annotator.py
@@ -29,10 +29,16 @@ class Annotator:
             if ids == doc_idx:
                 return self.annotations[idx]
 
-    def get_doc_mentions(self, doc_dict) -> str :
-        return doc_dict["mentions"]
+    def get_doc_mentions(self, doc_idx: int) -> dict :
+        for idx in range(len(self.annotations)):
+            ids = self.annotations[idx]["doc_idx"]
+            if ids == doc_idx:
+                return self.annotations[idx]["mentions"]
 
-    def get_doc_tokens(self, doc_dict) -> str :
-        return doc_dict["tokens"]
+    def get_doc_tokens(self, doc_idx: int) -> list :
+        for idx in range(len(self.annotations)):
+            ids = self.annotations[idx]["doc_idx"]
+            if ids == doc_idx:
+                return self.annotations[idx]["mentions"]
    
             
--- group.py
+++ group.py
@@ -1,10 +1,14 @@
 from annotator import Annotator
-
+import functions as fun
 
 class Group_Metrics :
 
     def __init__(self, annotator1 : Annotator, annotator2:Annotator, annotator3:Annotator, annotator4:Annotator):
 
+        self.annotator1 = annotator1
+        self.annotator2 = annotator2
+        self.annotator3 = annotator3
+        self.annotator4 = annotator4
         self.annotator1_doc_ids = annotator1.get_doc_idxs()
         self.annotator2_doc_ids = annotator2.get_doc_idxs()
         self.annotator3_doc_ids = annotator3.get_doc_idxs()
@@ -14,7 +18,11 @@ class Group_Metrics :
         self.same_docs = []
         self.same_mentions1 = []
         self.same_mentions2 = []
-
+        self.token_list = []
+        self.individual_mention_list = []
+        self.individual_token_list = []
+        self.group_mention_list = []
+        self.group_token_list = []
 
     def get_same_doc_ids(self) :        
         for i in range(len(self.annotator1_doc_ids)):
@@ -34,17 +42,41 @@ class Group_Metrics :
                     break
         return self.same_docs
 
-    def get_same_mentions(self):
-        for i in self.same_docs:
-            for j in self.annotator1_doc_ids:
-                if i == j:
-                    for k in range(len(self.annotator1.data)):
-                        if self.annotator1.data[k]["doc_idx"] == i:
-                            self.same_mentions1.append()
-        for i in self.same_docs:
-            for j in self.annotator2_doc_ids:
-                if i == j:
-                    self.same_mentions2.append(self.annotator2_doc_ids[i]["mentions"])
+    def get_group_words(self):
+        i = 40
+        mention1 = self.annotator1.get_doc_mentions(i)
+        token1 = self.annotator1.get_doc_tokens(i)
+        annotated1 = fun.item_class(token1, mention1)  
+        
+        mention2 = self.annotator2.get_doc_mentions(i)
+        token2 = self.annotator2.get_doc_tokens(i)
+        annotated2 = fun.item_class(token2, mention2)
+
+        mention3 = self.annotator3.get_doc_mentions(i)
+        token3 = self.annotator3.get_doc_tokens(i)
+        annotated3 = fun.item_class(token3, mention3)
+
+        mention4 = self.annotator4.get_doc_mentions(i)
+        token4 = self.annotator4.get_doc_tokens(i)
+        annotated4 = fun.item_class(token4, mention4)
+
+        return fun.majority_item(annotated1, annotated2, annotated3, annotated4)            
+ 
+
+
+                
+                
+                
+
+
+
+
+
+
+
+
+
+
 
     
 
