42b4d703565665a2e86d1e86e4d36fc36fe37615
danielsantosa <55735053+danielsantosa@users.noreply.github.com>
2022-10-09 13:34:28 +0800

    Fixed bugs in get_doc_metrics

--- label.py
+++ label.py
@@ -1,11 +1,17 @@
 from annotator import Annotator
-import functions as fun
 from collections import Counter
 
-class Group_Metrics :
+class Label_Metrics :
 
     def __init__(self, annotator1 : Annotator, annotator2:Annotator, annotator3:Annotator, annotator4:Annotator):
+        """
+            Class for obtaining the annotator individual label metrics 
 
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
         self.annotator1 = annotator1
         self.annotator2 = annotator2
         self.annotator3 = annotator3
@@ -15,21 +21,31 @@ class Group_Metrics :
         self.annotator3_doc_ids = annotator3.get_doc_idxs()
         self.annotator4_doc_ids = annotator4.get_doc_idxs()
         self.same_docs = []
-        self.same_mentions1 = []
-        self.same_mentions2 = []
-        self.token_list = []
-        self.individual_mention_list = []
-        self.individual_token_list = []
-        self.group_mention_list = []
-        self.group_token_list = []
 
-    def get_same_doc_ids(self) :        
+    def get_same_doc_ids(self) : 
+        """
+            Gets all the same annotated document ids for all the annotators
+
+            Returns:
+                The same annotated document ids annotated by all the annotators 
+              
+        """       
         list1 = self.get_common_files(self.annotator1_doc_ids,self.annotator2_doc_ids)
         list2 = self.get_common_files(self.annotator3_doc_ids,self.annotator4_doc_ids)
         self.same_docs = self.get_common_files(list1,list2)
         return self.same_docs
     
-    def get_common_files(self, doc_ids1 : list, doc_ids2: list):
+    def get_common_files(self, doc_ids1 : list, doc_ids2: list) -> list:
+        """
+            Gets all the same document ids for two annotators
+
+            Parameters:
+                doc_idx :
+                    The document ids of two annotators
+            Returns:
+                The same document ids shared by two annotators 
+              
+        """
         doc_list = []
         for i in range(len(doc_ids1)):
             for j in range(len(doc_ids2)):
@@ -38,7 +54,15 @@ class Group_Metrics :
                     break
         return doc_list 
     
-    def get_token_label(self, tokens, mentions):
+    def get_token_label(self, tokens:list, mentions:list) -> list:
+        """
+            Gets the  
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
         annotations_list1 = []
         annotations_list2 = []
         for ment in mentions:
@@ -46,12 +70,20 @@ class Group_Metrics :
             end = ment["end"]
             token = tokens[start:end]
             label = ment["labels"]
-            token = ' '.join(map(str, token))
+            token = self.list_To_String(token)
             annotations_list1 = [token, label, start, end]    
             annotations_list2.append(annotations_list1)    
         return annotations_list2
     
     def get_corpus_metrics(self):
+        """
+            Class for obtaining the individual annotator label metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
         annotated_doc = []
         annotated_corpus = []
 
@@ -81,18 +113,26 @@ class Group_Metrics :
             annotated_doc.clear()
         return annotated_corpus
 
-    def get_doc_metrics(self, group_annotated_doc) :
+    def get_doc_metrics(self, group_annotated_doc: list) -> list :
+        """
+            Class for obtaining the individual annotator label metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
         token_labels = []
         doc_metrics = []
         longest_tokens, number = self.get_longest_annotation_number(group_annotated_doc)
         for i in range(len(group_annotated_doc)):
             if i != number:
                 group_annotated_doc[i] = self.add_padding(longest_tokens, group_annotated_doc[i])
-        longest_tokens = group_annotated_doc[number]
+        longest_tokens_annotator = group_annotated_doc[number]
         group_annotated_doc.pop(number)
-        for i in range(len(longest_tokens)):
-            longest_token = longest_tokens[i][0]
-            longest_label = self.list_To_String(longest_tokens[i][1])
+        for i in range(len(longest_tokens_annotator)):
+            longest_token = longest_tokens_annotator[i][0]
+            longest_label = self.list_To_String(longest_tokens_annotator[i][1])
             token_labels.append(longest_label)
             for annotator in group_annotated_doc:
                 for annotated_token in annotator:
@@ -106,8 +146,30 @@ class Group_Metrics :
             token_labels.clear()
             doc_metrics.append(token_majority_label_metric)
         return doc_metrics
+    
+    def get_annotator_metrics(self, annotated_corpus):
+        """
+            Class for obtaining the individual annotator label metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
+        for annotated_document in annotated_corpus:
+            print("new doc")
+            for token_agreement in annotated_document:
+                print(token_agreement[2])
  
-    def get_majority_label(self, List) :
+    def get_majority_label(self, List: list) -> str:
+        """
+            Class for obtaining the individual annotator label metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
         counter = 0
         num = List[0]     
         for i in List:
@@ -117,7 +179,15 @@ class Group_Metrics :
             num = i 
         return num 
 
-    def get_all_labels_same(self, List) :
+    def get_all_labels_same(self, List: list) -> bool:
+        """
+            Class for obtaining the individual annotator label metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
         for i in range(len(List)-1):
             if List[i] == List[i+1]:
                 continue
@@ -125,19 +195,41 @@ class Group_Metrics :
                 return False
         return True
 
-    def list_To_String(self,List):    
-        # initialize an empty string
+    def list_To_String(self, List: list) -> str:
+        """
+            Class for obtaining the individual annotator label metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """    
         str1 = " "    
-        # return string 
         return (str1.join(List))
 
-    def add_padding(self, length, List):
+    def add_padding(self, length:int, List: list) -> list:
+        """
+            Class for obtaining the individual annotator label metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
         diff_len = length - len(List)
         if diff_len < 0:
             raise AttributeError('Length error list is too long')
         return List + ["null empty"] * diff_len       
 
-    def get_longest_annotation_number(self, group_annotated_doc) :
+    def get_longest_annotation_number(self, group_annotated_doc:list) -> int :
+        """
+            Class for obtaining the individual annotator label metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
         longest = 0
         for i in range(len(group_annotated_doc)):
             length = len(group_annotated_doc[i])                 
---
+++ ngram.py
@@ -0,0 +1,210 @@
+from annotator import Annotator
+from collections import Counter
+from statistics import multimode
+
+class Ngram_Metrics :
+
+    def __init__(self, annotator1 : Annotator, annotator2:Annotator, annotator3:Annotator, annotator4:Annotator):
+        """
+            Class for obtaining the individual ngram metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
+        self.annotator1 = annotator1
+        self.annotator2 = annotator2
+        self.annotator3 = annotator3
+        self.annotator4 = annotator4
+        self.annotator1_doc_ids = annotator1.get_doc_idxs()
+        self.annotator2_doc_ids = annotator2.get_doc_idxs()
+        self.annotator3_doc_ids = annotator3.get_doc_idxs()
+        self.annotator4_doc_ids = annotator4.get_doc_idxs()
+        self.same_docs = []
+        self.annotator_count = 4
+
+    def get_same_doc_ids(self) : 
+        """
+            Gets all the same annotated document ids for all the annotators
+
+            Returns:
+                The same annotated document ids annotated by all the annotators 
+              
+        """       
+        list1 = self.get_common_files(self.annotator1_doc_ids,self.annotator2_doc_ids)
+        list2 = self.get_common_files(self.annotator3_doc_ids,self.annotator4_doc_ids)
+        self.same_docs = self.get_common_files(list1,list2)
+        return self.same_docs
+    
+    def get_common_files(self, doc_ids1 : list, doc_ids2: list) -> list:
+        """
+            Gets all the same document ids for two annotators
+
+            Parameters:
+                doc_idx :
+                    The document ids of two annotators
+            Returns:
+                The list of same document ids shared by two annotators 
+              
+        """
+        doc_list = []
+        for i in range(len(doc_ids1)):
+            for j in range(len(doc_ids2)):
+                if doc_ids1[i] == doc_ids2[j]:
+                    doc_list.append(doc_ids1[i])  
+                    break
+        return doc_list 
+    
+    def get_token_label(self, tokens:list, mentions:list) -> list:
+        """
+            Gets the  
+
+            Parameters:
+                tokens :
+                    The list of tokens in a document id
+                mentions : 
+                    The list of mentions in a document id
+            Returns:
+                The same document ids shared by two annotators 
+              
+        """
+        annotations_list1 = []
+        annotations_list2 = []
+        for ment in mentions:
+            start = ment["start"]
+            end = ment["end"]
+            ngram = end - start
+            token = tokens[start:end]
+            label = ment["labels"]
+            token = self.list_To_String(token)
+            annotations_list1 = [token, label, [start, end, ngram]]    
+            annotations_list2.append(annotations_list1)    
+        return annotations_list2
+    
+    def get_corpus_metrics(self):
+        """
+            Class for obtaining the individual annotator label metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
+        annotated_doc = []
+        annotated_corpus = []
+
+        for i in self.same_docs:
+            mention1 = self.annotator1.get_doc_mentions(i)
+            token1 = self.annotator1.get_doc_tokens(i)
+            annotated1 = self.get_token_label(token1, mention1)  
+            annotated_doc.append(annotated1)
+
+            mention2 = self.annotator2.get_doc_mentions(i)
+            token2 = self.annotator2.get_doc_tokens(i)
+            annotated2 = self.get_token_label(token2, mention2)
+            annotated_doc.append(annotated2)
+
+            mention3 = self.annotator3.get_doc_mentions(i)
+            token3 = self.annotator3.get_doc_tokens(i)
+            annotated3 = self.get_token_label(token3, mention3)
+            annotated_doc.append(annotated3)
+
+            mention4 = self.annotator4.get_doc_mentions(i)
+            token4 = self.annotator4.get_doc_tokens(i)
+            annotated4 = self.get_token_label(token4, mention4)
+            annotated_doc.append(annotated4)
+
+            doc_metrics = self.get_doc_metrics(annotated_doc)
+            annotated_corpus.append(doc_metrics)
+            annotated_doc.clear()
+        return annotated_corpus[0]
+
+    def get_doc_metrics(self, group_annotated_doc: list) -> list :
+        """
+            Class for obtaining the individual annotator label metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
+        token_labels = []
+        doc_metrics = []
+        majority_label = ''
+        majority_label_count = 0
+
+        for orig_annotator in group_annotated_doc:
+            for orig_annotated_ngram in orig_annotator: 
+                token_labels.clear()                       
+                count_ngram = 1
+                count_label = 1
+                orig_token = orig_annotated_ngram[0]
+                orig_label = orig_annotated_ngram[1]
+                if type(orig_label) == list:
+                    orig_label = self.list_To_String(orig_label)
+                orig_ngram = orig_annotated_ngram[2]
+                for other_annotator in group_annotated_doc:
+                    if orig_annotator != other_annotator:
+                        for other_annotated_ngram in other_annotator:
+                            if orig_ngram == other_annotated_ngram[2]:
+                                count_ngram += 1
+                                other_label = self.list_To_String(other_annotated_ngram[1])
+                                token_labels.append(other_label)
+                                break
+                if count_ngram < 2:
+                    orig_ngram_ratio = count_ngram/ self.annotator_count
+                    label_ratio = "N/A"
+                    single_ngram = [token_labels, orig_ngram, orig_ngram_ratio, orig_token, orig_label, label_ratio]
+                    doc_metrics.append(single_ngram)
+                else :
+                    if not self.search_ngram_in_list(orig_ngram, doc_metrics):
+                        majority_label = multimode(token_labels)
+                        if type(majority_label) == list:
+                            majority_label = self.list_To_String(majority_label)
+                        majority_label_count = self.get_majority_label_count(token_labels, majority_label)
+                        # Calculate the percentage ratio of ngrams 
+                        ngram_ratio = count_ngram/ self.annotator_count                                     
+                        # Calculate the percentage ratio of majority label
+                        label_ratio = (majority_label_count + count_label)/ self.annotator_count
+                        ngram_metrics = [token_labels, orig_ngram, ngram_ratio, orig_token, majority_label_count, majority_label, label_ratio]
+                        doc_metrics.append(ngram_metrics)   
+                    else:
+                        continue
+        return doc_metrics
+    
+    # def get_annotator_metrics(self, annotated_corpus):
+    #     """
+    #         Class for obtaining the individual annotator label metrics 
+
+    #         Parameters:
+    #             annotators :
+    #                 Instance of Annotator class for a person
+              
+    #     """
+    #     for annotated_document in annotated_corpus:
+    #         print("new doc")
+    #         for token_agreement in annotated_document:
+    #             print(token_agreement[2])
+ 
+    def get_majority_label_count(self, List:list, majority_label: str) -> int:
+        counter = 0
+        for label in List:
+            if label in majority_label:
+                counter += 1
+        return counter
+ 
+    def list_To_String(self, List: list) -> str:
+        """
+            Class for obtaining the individual annotator label metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """    
+        str1 = " "    
+        return (str1.join(List))
+
+     
+                
