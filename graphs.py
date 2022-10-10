7239f2d9397a6657ef0d962683891b3ca463febd
danielsantosa <55735053+danielsantosa@users.noreply.github.com>
2022-10-10 11:47:35 +0800

    Added multiple pie charts in graphs.py

--- graphs.py
+++ graphs.py
@@ -2,45 +2,46 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def create_pie_chart(label_list):
-    # Creating dataset
-    labels = ['FULL AGREEMENT','PARTIAL AGREEMENT']
-    
-    data = [label_list[1], label_list[2]]    
-    
-    # Creating explode data
-    explode = (0.1, 0.0)
-    
-    # Creating color parameters
-    colors = ( "beige", "cyan")
-    
-    # Wedge properties
-    wp = { 'linewidth' : 1, 'edgecolor' : "green" }
-    
-    # Creating autocpt arguments
-    def func(pct, allvalues):
-        absolute = int(pct / 100.*np.sum(allvalues))
-        return "{:.1f}%\n({:d} g)".format(pct, absolute)
-    
-    # Creating plot
-    fig, ax = plt.subplots(figsize =(10, 7))
-    wedges, texts, autotexts = ax.pie(data,
-                                    autopct = lambda pct: func(pct, data),
-                                    explode = explode,
-                                    labels = labels,
-                                    shadow = True,
-                                    colors = colors,
-                                    startangle = 90,
-                                    wedgeprops = wp,
-                                    textprops = dict(color ="magenta"))
-    
-    # Adding legend
-    ax.legend(wedges, labels,
-            title = label_list[0],
-            loc ="center left",
-            bbox_to_anchor =(1, 0, 0.5, 1))
-    
-    plt.setp(autotexts, size = 8, weight ="bold")
-    ax.set_title("Individual Label Annotation Metrics")
-    
-    # show plot
-    plt.show()
+    for label_data in label_list:
+        # Creating dataset
+        label = ['FULL AGREEMENT','PARTIAL AGREEMENT']
+        
+        data = [label_data[1], label_data[2]]    
+        
+        # Creating explode data
+        explode = (0.1, 0.0)
+        
+        # Creating color parameters
+        colors = ( "beige", "cyan")
+        
+        # Wedge properties
+        wp = { 'linewidth' : 1, 'edgecolor' : "green" }
+        
+        # Creating autocpt arguments
+        def func(pct, allvalues):
+            absolute = int(pct / 100.*np.sum(allvalues))
+            return "{:.1f}%\n({:d} g)".format(pct, absolute)
+        
+        # Creating plot
+        fig, ax = plt.subplots(figsize =(10, 7))
+        wedges, texts, autotexts = ax.pie(data,
+                                        autopct = lambda pct: func(pct, data),
+                                        explode = explode,
+                                        labels = label,
+                                        shadow = True,
+                                        colors = colors,
+                                        startangle = 90,
+                                        wedgeprops = wp,
+                                        textprops = dict(color ="magenta"))
+        
+        # Adding legend
+        ax.legend(wedges, label,
+                title = label_data[0],
+                loc ="center left",
+                bbox_to_anchor =(1, 0, 0.5, 1))
+        
+        plt.setp(autotexts, size = 8, weight ="bold")
+        ax.set_title("Individual Label Annotation Metrics")
+        
+        # show plot
+        plt.show()
