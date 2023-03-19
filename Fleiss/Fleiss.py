65ffaa0b7c9cbb80977f0a0a6866adff05b2b8c4
danielsantosa <55735053+danielsantosa@users.noreply.github.com>
2023-03-19 18:15:57 +0800

    Added Krippendorff Alpha Calculations

--- Fleiss/Fleiss.py
+++ Fleiss/Fleiss.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+import krippendorff
 from annotator import Annotator
 from statistics import multimode
 
@@ -73,126 +76,121 @@ class Label_Metrics :
             annotations_list1 = [token, label, start, end]    
             annotations_list2.append(annotations_list1)    
         return annotations_list2
-    
-    def get_corpus_metrics(self):
+
+    def get_all_annotators_tokens_labels_single_doc(self, doc_idx) -> pd.DataFrame:
         """
-            Gets the calculated label corpus metrics from all the documents from the group
-
-            Returns:
-                The calculated label corpus metrics for all documents annotated by the group
-              
-        """
-        annotated_doc = []
-
-        self.get_same_doc_ids()
-
-        for i in self.same_docs:  
-            for annotator in self.annotator_list:
-                mention = annotator.get_doc_mentions(i)
-                token = annotator.get_doc_tokens(i)
-                annotated = self.get_token_label(token, mention)
-                annotated_doc.append(annotated)
-            single_doc_metrics = self.get_single_doc_metrics(annotated_doc)
-            self.annotated_corpus.append(single_doc_metrics)
-            annotated_doc.clear()
-
-    def get_single_doc_metrics(self, group_annotated_doc: list) -> list :
-        """
-            Gets the calculated label corpus metrics from all the same documents from the group
+            Gets the tokens and labels for a doc_idx for all the annotators
 
             Parameters:
-                group_annotated_doc :
-                    The annotated group documents containing tokens with labels for the same document
-                    
+                doc_idx :
+                    the document id
+
             Returns:
-                The calculated corpus label metrics for a single document
-              
+                The tokens with labels as a DataFrame for all the annotators
+
         """
-        token_labels = []
-        doc_metrics = []
-        longest_tokens, number = self.get_longest_annotation_number(group_annotated_doc)
-        for i in range(len(group_annotated_doc)):
-            if i != number:
-                group_annotated_doc[i] = self.add_padding(longest_tokens, group_annotated_doc[i])
-        longest_tokens_annotator = group_annotated_doc[number]
-        group_annotated_doc.pop(number)
-        for i in range(len(longest_tokens_annotator)):            
-            longest_token = longest_tokens_annotator[i][0]
-            longest_label = longest_tokens_annotator[i][1]
-            token_labels.append(longest_label)
-            for annotator in group_annotated_doc:
-                for annotated_token in annotator:
-                    if longest_token in annotated_token[0]:
-                        label = annotated_token[1]
-                        token_labels.append(label)
-                        break            
-            majority_label = self.list_To_String(multimode(token_labels))            
-            all_labels_same = self.get_all_labels_same(token_labels)
-            token_majority_label_metric = [longest_token, majority_label, all_labels_same]
-            token_labels.clear()
-            doc_metrics.append(token_majority_label_metric)
-        return doc_metrics
-    
-    def get_label_metrics(self) -> list:
+        # Initialize an empty DataFrame with the desired columns
+        annotated_df = pd.DataFrame(columns=['annotator_id', 'token', 'label', 'start', 'end'])
+
+        # Loop through all the annotators
+        for annotator in self.annotator_list:
+            # Get the annotator_id from the annotator object (assuming it has an 'id' attribute)
+            annotator_id = annotator.name
+            mention = annotator.get_doc_mentions(doc_idx)
+            token = annotator.get_doc_tokens(doc_idx)
+            annotated = self.get_token_label(token, mention)
+
+            # Create a temporary DataFrame to store the current annotator's data
+            temp_df = pd.DataFrame(annotated, columns=['token', 'label', 'start', 'end'])
+            temp_df['annotator_id'] = annotator_id
+
+            # Append the temporary DataFrame to the main DataFrame using pandas.concat
+            annotated_df = pd.concat([annotated_df, temp_df], ignore_index=True)
+
+        return annotated_df
+
+
+    def create_krippendorff_alpha_table(self, annotated_df: pd.DataFrame) -> pd.DataFrame:
+        # Pivot the annotated_df DataFrame to create a table with annotators as rows and tokens as columns
+        krippendorff_alpha_table = annotated_df.pivot_table(index='annotator_id', columns='token', values='label', aggfunc='first')
+
+        # Ensure the table contains dtype 'object' and missing values are replaced with None
+        krippendorff_alpha_table = krippendorff_alpha_table.astype(object).where(pd.notnull(krippendorff_alpha_table), None)
+
+        return krippendorff_alpha_table
+
+    def convert_krippendorff_alpha_table_to_list_of_lists(self, krippendorff_alpha_table: pd.DataFrame) -> list:
         """
-            Gets the label category metrics for corpus 
+            Convert a DataFrame to a list of lists
 
             Parameters:
-                annotated_corpus :
-                    The annotated corpus containing processed label metrics
-          
-            Returns : 
-                The calculated metrics for all the label             
-        """
-        label_list = self.get_all_labels()
-        label_metrics_list = []
-        counter_false = 0
-        counter_true = 0
-        for label in label_list :
-            for annotated_document in self.annotated_corpus:
-                for token_agreement in annotated_document:
-                    if token_agreement[1] == label:
-                        if token_agreement[2] == True:
-                            counter_true += 1
-                        if token_agreement[2] == False:
-                            counter_false += 1
-            label_metrics = [label, counter_true, counter_false]
-            label_metrics_list.append(label_metrics)
-        return label_metrics_list
+                df :
+                    The input DataFrame
 
-    def get_all_labels(self) -> list:
-        """
-            Gets all the labels in the annotated corpus 
-                  
             Returns:
-                All the labels in the annotated corpus in a list 
-              
+                A list of lists representation of the input DataFrame
         """
-        label_list = []
-        for document_metrics in self.annotated_corpus :
-            for metrics in document_metrics:
-                if metrics[1] not in label_list:
-                    label_list.append(metrics[1])
-        return label_list
- 
-    def get_all_labels_same(self, Label_List: list) -> bool:
+        return krippendorff_alpha_table.values.tolist()
+
+    def convert_labels_to_integers(self, data_list: list) -> list:
         """
-            Gives a boolean True if all labels are the same and False if not all the same for a token 
+            Convert labels in a list of lists to integers and replace None with NaN
 
             Parameters:
-                Label_List :
-                    The list of labels annotated for a single token by the group
-                    
+                data_list :
+                    The input list of lists
+
             Returns:
-                True if all labels are the same and False if not all the same 
-              
+                A list of lists with labels converted to integers and None replaced with NaN
         """
-        for i in range(len(Label_List)-1):
-            if Label_List[i] == Label_List[i+1]:
-                continue
-            else:
-                return False
-        return True
+        # Get unique labels from the data_list (excluding None)
+        unique_labels = set(x for sublist in data_list for x in sublist if x is not None)
+
+        # Create a mapping of labels to integers
+        label_to_int = {label: idx for idx, label in enumerate(unique_labels, 1)}
+
+        # Apply the mapping to the list of lists
+        converted_data_list = [[label_to_int.get(x, np.nan) if x is not None else np.nan for x in sublist] for sublist in data_list]
+
+        return converted_data_list
+
+    def calculate_krippendorff_alpha_for_all_docs(self) -> float:
+        """
+            Calculate Krippendorff's alpha for all documents
+
+            Returns:
+                The Krippendorff's alpha value for all documents
+        """
+        # Get the same document ids annotated by all the annotators
+        same_docs = self.get_same_doc_ids()
+
+        # Initialize a list to store the Krippendorff's alpha values for each document
+        krippendorff_alpha_values = []
+
+        # Loop through all the documents
+        for doc_idx in same_docs:
+            # Get the tokens and labels for a doc_idx for all the annotators
+            annotated_df = self.get_all_annotators_tokens_labels_single_doc(doc_idx)
+
+            # Create a table with annotators as rows and tokens as columns
+            krippendorff_alpha_table = self.create_krippendorff_alpha_table(annotated_df)
+
+            # Convert the table to a list of lists
+            data_list = self.convert_krippendorff_alpha_table_to_list_of_lists(krippendorff_alpha_table)
+
+            # Convert labels to integers and replace None with NaN
+            converted_data_list = self.convert_labels_to_integers(data_list)
+
+            # Calculate Krippendorff's alpha
+            krippendorff_alpha = krippendorff.alpha(converted_data_list, level_of_measurement='nominal')
+
+            # Add the Krippendorff's alpha value to the list
+            krippendorff_alpha_values.append(krippendorff_alpha)
+
+        # Calculate the mean Krippendorff's alpha value
+        mean_krippendorff_alpha = np.mean(krippendorff_alpha_values)
+
+        return mean_krippendorff_alpha
 
     def list_To_String(self, List: list) -> str:
         """
@@ -204,49 +202,10 @@ class Label_Metrics :
                     
             Returns:
                 The converted object from list into type string
-              
+                
         """    
         str1 = " "    
         return (str1.join(List))
-
-    def add_padding(self, list_length:int, token_list: list) -> list:
-        """
-            Adds padding to shorter list of tokens in order to be able to loop through all lists together
-
-            Parameters:
-                list_length :
-                    The desired length of the list to add padding to
-                token_list : 
-                    The token list to add padding into the desired list length
-                    
-            Returns:
-                The token list with padding into the desired list length 
-              
-        """
-        diff_len = list_length - len(token_list)
-        if diff_len < 0:
-            raise AttributeError('Length error list is too long')
-        return token_list + ["null empty"] * diff_len       
-
-    def get_longest_annotation_number(self, group_annotated_token_doc:list) -> int :
-        """
-            Gets the longest list length of the token in the group annotated document 
-
-            Parameters:
-                group_annotated_token_doc :
-                    The group annotation tokens for a document 
-                    
-            Returns:
-                The longest list length of token in the document
-              
-        """
-        longest = 0
-        for i in range(len(group_annotated_token_doc)):
-            length = len(group_annotated_token_doc[i])                 
-            if length > longest:
-                longest = length
-                index = i
-        return longest, index
 
 
                 
