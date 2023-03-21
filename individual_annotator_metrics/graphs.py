5a01533a292b406a203340937028e031920da692
danielsantosa <55735053+danielsantosa@users.noreply.github.com>
2023-03-21 16:02:59 +0800

    Added individual annotator performance metrics

---
+++ individual_annotator_metrics/graphs.py
@@ -0,0 +1,144 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+def create_pie_chart(label_list):
+    for label_data in label_list:
+        # Creating dataset
+        label = ['FULL LABEL AGREEMENT','PARTIAL LABEL AGREEMENT']
+        
+        data = [label_data[1], label_data[2]]    
+        
+        # Creating explode data
+        explode = (0.1, 0.0)
+        
+        # Creating color parameters
+        colors = ( "blue", "magenta")
+        
+        # Wedge properties
+        wp = { 'linewidth' : 1, 'edgecolor' : "green" }
+        
+        # Creating autocpt arguments
+        def func(pct, allvalues):
+            absolute = int(pct / 100.*np.sum(allvalues))
+            return "{:.1f}%\n({:d} g)".format(pct, absolute)
+        
+        # Creating plot
+        fig, ax = plt.subplots(figsize =(10, 7))
+        wedges, texts, autotexts = ax.pie(data,
+                                        autopct = lambda pct: func(pct, data),
+                                        explode = explode,
+                                        labels = label,
+                                        shadow = True,
+                                        colors = colors,
+                                        startangle = 90,
+                                        wedgeprops = wp,
+                                        textprops = dict(color ="black"))
+        
+        # Adding legend
+        ax.legend(wedges, label,
+                title = label_data[0],
+                loc ="center left",
+                bbox_to_anchor =(1, 0, 0.5, 1))
+        
+        plt.setp(autotexts, size = 8, weight ="bold")
+        ax.set_title("Individual Label Annotation Metrics")
+        
+        # show plot
+        plt.show()
+
+def create_pie_chart_ngram(ngram_list):
+    for ngram_data in ngram_list:
+        # Creating dataset
+        label = ['FULL NGRAM AGREEMENT','PARTIAL NGRAM AGREEMENT']
+        
+        data = [ngram_data[1], ngram_data[2]]    
+        
+        # Creating explode data
+        explode = (0.1, 0.0)
+        
+        # Creating color parameters
+        colors = ( "blue", "magenta")
+        
+        # Wedge properties
+        wp = { 'linewidth' : 1, 'edgecolor' : "green" }
+        
+        # Creating autocpt arguments
+        def func(pct, allvalues):
+            absolute = int(pct / 100.*np.sum(allvalues))
+            return "{:.1f}%\n({:d} g)".format(pct, absolute)
+        
+        # Creating plot
+        fig, ax = plt.subplots(figsize =(10, 7))
+        wedges, texts, autotexts = ax.pie(data,
+                                        autopct = lambda pct: func(pct, data),
+                                        explode = explode,
+                                        labels = label,
+                                        shadow = True,
+                                        colors = colors,
+                                        startangle = 90,
+                                        wedgeprops = wp,
+                                        textprops = dict(color ="black"))
+        
+        # Adding legend
+        ax.legend(wedges, label,
+                title = ngram_data[0],
+                loc ="center left",
+                bbox_to_anchor =(1, 0, 0.5, 1))
+        
+        plt.setp(autotexts, size = 8, weight ="bold")
+        ax.set_title("Ngram Type Annotation Metrics")
+        
+        # show plot
+        plt.show()
+
+def create_pie_chart_individual_ngram(individual_ngram_metrics):
+        # Creating dataset
+        label = ['FULL NGRAM AGREEMENT','PARTIAL NGRAM AGREEMENT']
+        
+        data = [individual_ngram_metrics[1], individual_ngram_metrics[2]]    
+        
+        # Creating explode data
+        explode = (0.1, 0.0)
+        
+        # Creating color parameters
+        colors = ( "silver", "orchid")
+        
+        # Wedge properties
+        wp = { 'linewidth' : 1, 'edgecolor' : "green" }
+        
+        # Creating autocpt arguments
+        def func(pct, allvalues):
+            absolute = int(pct / 100.*np.sum(allvalues))
+            return "{:.1f}%\n({:d} g)".format(pct, absolute)
+        
+        # Creating plot
+        fig, ax = plt.subplots(figsize =(10, 7))
+        wedges, texts, autotexts = ax.pie(data,
+                                        autopct = lambda pct: func(pct, data),
+                                        explode = explode,
+                                        labels = label,
+                                        shadow = True,
+                                        colors = colors,
+                                        startangle = 90,
+                                        wedgeprops = wp,
+                                        textprops = dict(color ="black"))
+        
+        # Adding legend
+        ax.legend(wedges, label,
+                title = individual_ngram_metrics[0],
+                loc ="center left",
+                bbox_to_anchor =(1, 0, 0.5, 1))
+        
+        plt.setp(autotexts, size = 8, weight ="bold")
+        ax.set_title("Individual Annotator Ngram Annotation Metrics")
+        
+        # show plot
+        plt.show()
+
+def create_heatmap(ngram_metrics):
+    df = pd.DataFrame(ngram_metrics, columns =['Label', 'Ngram', 'Metric'])
+    df2 = df.sort_values(by="Metric", ascending=True)
+    df3 = df2.pivot("Label", "Ngram",values='Metric')
+    sns.heatmap(df3,cmap='RdYlGn', annot=True)
---
+++ individual_annotator_metrics/individual_annotator_metrics.py
@@ -0,0 +1,183 @@
+import pandas as pd
+import numpy as np
+from collections import Counter
+from annotator import Annotator
+
+
+class Label_Metrics :
+
+    def __init__(self, *args):
+        """
+            Class for obtaining the annotator individual label metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
+        self.annotator_list = list(args)
+        self.annotator_count = len(self.annotator_list)
+        self.annotator_numbered_list = []
+        self.same_docs = []
+        self.annotated_corpus = []
+
+    def get_same_doc_ids(self) : 
+        """
+            Gets all the same annotated document ids for all the annotators
+
+            Returns:
+                The same annotated document ids annotated by all the annotators 
+              
+        """ 
+        # find shortest document
+        shortest_doc_length = float('inf')    
+        shortest_doc_id = 0
+        shortest_annotator = None
+        doc_idxs1 = []
+        doc_idxs2 = []
+        for i, annotator in enumerate(self.annotator_list):
+            doc_length = len(annotator.get_doc_idxs())
+            if doc_length < shortest_doc_length:
+                shortest_doc_length = doc_length
+                shortest_annotator = self.annotator_list[i]
+                shortest_doc_id = i
+        doc_idxs1 = shortest_annotator.get_doc_idxs()
+        self.same_docs = set(doc_idxs1)
+        # compare the shortest document with all the other documents to get same documents
+        for i, annotator in enumerate(self.annotator_list):            
+            if shortest_doc_id != i:
+                doc_idxs2 = annotator.get_doc_idxs()
+                self.same_docs = self.same_docs.intersection(doc_idxs2)
+        return list(self.same_docs)
+    
+    def get_token_label(self, tokens:list, mentions: dict) -> list:
+        """
+            Gets the combined token, labels, and gets the correct position of tokens
+
+            Parameters:
+                tokens :
+                    The list of tokens for a document id
+                mentions : 
+                    The dictionary of mentions for a document id
+                    
+            Returns:
+                The combined tokens with labels as a list for a document id 
+              
+        """
+        annotations_list1 = []
+        annotations_list2 = []
+        for ment in mentions:
+            start = ment["start"]
+            end = ment["end"]
+            token = tokens[start:end]
+            token = self.list_To_String(token)
+            label = ment["labels"]
+            label = self.list_To_String(label)
+            annotations_list1 = [token, label, start, end]    
+            annotations_list2.append(annotations_list1)    
+        return annotations_list2
+
+    def get_all_annotators_tokens_labels_all_docs(self) -> pd.DataFrame:
+        """
+            Gets the tokens and labels for a doc_idx for all the annotators
+
+            Parameters:
+                doc_idx :
+                    the document id
+
+            Returns:
+                The tokens with labels as a DataFrame for all the annotators
+
+        """
+        # Initialize an empty DataFrame with the desired columns
+        annotated_df = pd.DataFrame(columns=['annotator_id', 'token', 'label', 'start', 'end'])
+
+        self.get_same_doc_ids()
+    
+        # Loop through all documents
+        for doc_idx in self.same_docs:
+            # Loop through all the annotators
+            for annotator in self.annotator_list:
+                # Get the annotator_id from the annotator object (assuming it has an 'id' attribute)
+                annotator_id = annotator.name
+                mention = annotator.get_doc_mentions(doc_idx)
+                token = annotator.get_doc_tokens(doc_idx)
+                annotated = self.get_token_label(token, mention)
+
+                # Create a temporary DataFrame to store the current annotator's data
+                temp_df = pd.DataFrame(annotated, columns=['token', 'label', 'start', 'end'])
+                temp_df['annotator_id'] = annotator_id
+
+                # Append the temporary DataFrame to the main DataFrame using pandas.concat
+                annotated_df = pd.concat([annotated_df, temp_df], ignore_index=True)
+
+        return annotated_df
+
+    def create_individual_text_metrics_table(self, annotated_df: pd.DataFrame) -> pd.DataFrame:
+        # Pivot the annotated_df DataFrame to create a table with tokens as rows and annotators as columns
+        krippendorff_alpha_table = annotated_df.pivot_table(index='token', columns='annotator_id', values='label', aggfunc='first')
+
+        # Replace missing values with np.nan
+        krippendorff_alpha_table = krippendorff_alpha_table.replace(to_replace=[None], value=np.nan)
+
+        # Calculate the mode of each row in the DataFrame
+        mode_series = krippendorff_alpha_table.apply(lambda x: x.value_counts().idxmax() if x.value_counts().max() > 1 else 0, axis=1)
+
+        # Calculate the percentage agreement for each row (token) in the DataFrame
+        boolean_df = krippendorff_alpha_table.values == mode_series.values[:, np.newaxis]
+        percentage_series = boolean_df.mean(axis=1) * 100   
+
+        krippendorff_alpha_table['percent_agreement'] = percentage_series
+
+        return krippendorff_alpha_table
+
+    def calculate_agreement(self, df: pd.DataFrame) -> dict:
+        agreement = {annotator: 0 for annotator in df.columns}
+
+        for token in df.index:
+            token_labels = df.loc[token].dropna()
+            majority_label = token_labels.value_counts().idxmax()
+
+            for annotator in df.columns:
+                current_label = df.loc[token, annotator]
+                if not pd.isna(current_label) and current_label == majority_label:
+                    agreement[annotator] += 1
+
+        total_tokens = len(df)
+        agreement_percentage = {annotator: (agreement[annotator] / total_tokens) * 100 for annotator in agreement}
+
+        return agreement_percentage
+
+    def list_To_String(self, List: list) -> str:
+        """
+            Converts a list into a string 
+
+            Parameters:
+                List :
+                    The object of type list to convert to string
+                    
+            Returns:
+                The converted object from list into type string
+                
+        """    
+        str1 = " "    
+        return (str1.join(List))
+
+
+                
+                
+                
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
