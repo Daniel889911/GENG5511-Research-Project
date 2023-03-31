0fa6964848ae1a8b89386c2b860da6cdcd441b54
danielsantosa <55735053+danielsantosa@users.noreply.github.com>
2023-03-31 11:15:27 +0800

    Added new label category methods

---
+++ label_categories/label_categories.py
@@ -0,0 +1,202 @@
+import pandas as pd
+import numpy as np
+from irrCAC.raw import CAC
+from annotator import Annotator
+
+
+class Label_Metrics :
+
+    def __init__(self, *args):
+        """
+            Class for obtaining the annotator individual label metrics 
+
+            Parameters:
+                annotators :
+                    Instance of Annotator class for a person
+              
+        """
+        self.annotator_list = list(args)
+        self.annotator_count = len(self.annotator_list)
+        self.annotator_numbered_list = []
+        self.same_docs = []
+        self.annotated_corpus = []
+        self.labels = ['Item', 'Activity', 'Location', 'Time', 'Attribute', 'Cardinality', 'Agent', 'Consumable', 'Observation/Observed_state', 'Observation/Quantitative', 'Observation/Qualitative', 'Specifier', 'Event', 'Unsure', 'Typo', 'Abbreviation']
+
+    def get_same_doc_ids(self): 
+        """
+            Gets all the same annotated document ids for all the annotators
+
+            Returns:
+                The same annotated document ids annotated by all the annotators 
+
+        """ 
+        # Initialize a set with the doc_idxs from the first annotator
+        same_docs = set(self.annotator_list[0].get_doc_idxs())
+
+        # Iterate through the remaining annotators, updating the set with the intersection
+        for annotator in self.annotator_list[1:]:
+            same_docs.intersection_update(annotator.get_doc_idxs())
+
+        # Store the same annotated document ids in the class variable
+        self.same_docs = same_docs
+        return self.same_docs
+    
+    def get_token_label(self, tokens:list, mentions: dict) -> list:
+        """
+            Gets the combined token, labels, and gets the correct position of tokens
+
+            Parameters:
+                tokens :
+                    The list of tokens for a document id
+                mentions : 
+                    The dictionary of mentions for a document id
+                    
+            Returns:
+                The combined tokens with labels as a list for a document id 
+              
+        """
+        annotations_list1 = []
+        annotations_list2 = []
+        for ment in mentions:
+            start = ment["start"]
+            end = ment["end"]
+            token = tokens[start:end]
+            token = self.list_To_String(token)
+            label = ment["labels"]
+            label = self.list_To_String(label)
+            annotations_list1 = [token, label, start, end]    
+            annotations_list2.append(annotations_list1)    
+        return annotations_list2
+
+    def get_all_annotators_tokens_labels_single_doc(self, doc_idx) -> pd.DataFrame:
+        """
+            Gets the tokens and labels for a doc_idx for all the annotators
+
+            Parameters:
+                doc_idx :
+                    the document id
+
+            Returns:
+                The tokens with labels as a DataFrame for all the annotators
+
+        """
+        # Initialize an empty DataFrame with the desired columns
+        annotated_df = pd.DataFrame(columns=['annotator_id', 'token', 'label', 'start', 'end'])
+
+        # Loop through all the annotators
+        for annotator in self.annotator_list:
+            # Get the annotator_id from the annotator object (assuming it has an 'id' attribute)
+            annotator_id = annotator.name
+            mention = annotator.get_doc_mentions(doc_idx)
+            token = annotator.get_doc_tokens(doc_idx)
+            annotated = self.get_token_label(token, mention)
+
+            # Create a temporary DataFrame to store the current annotator's data
+            temp_df = pd.DataFrame(annotated, columns=['token', 'label', 'start', 'end'])
+            temp_df['annotator_id'] = annotator_id
+
+            # Append the temporary DataFrame to the main DataFrame using pandas.concat
+            annotated_df = pd.concat([annotated_df, temp_df], ignore_index=True)
+
+        return annotated_df
+
+    def create_single_annotations_table(self, annotated_df: pd.DataFrame) -> pd.DataFrame:
+        # Pivot the annotated_df DataFrame to create a table with tokens as rows and annotators, labels as columns
+        table = annotated_df.pivot_table(index='token', columns='annotator_id', values='label', aggfunc='first')
+
+        # Ensure the table contains dtype 'object' and missing values are replaced with None
+        table = table.astype(object).where(pd.notnull(table), None)
+        return table
+
+    def get_accumulated_table(self) -> pd.DataFrame:
+        """
+            Get the accumulated table for all the documents
+
+            Returns:
+                The accumulated table for all the documents
+        """
+        # Get the same document ids annotated by all the annotators
+        same_docs = self.get_same_doc_ids()
+
+        # Initialize an empty dataframe to accumulate all subdocuments' annotations
+        accumulated_table = pd.DataFrame()
+        for doc_idx in same_docs:
+            # Get the tokens and labels for a doc_idx for all the annotators
+            annotated_df = self.get_all_annotators_tokens_labels_single_doc(doc_idx)
+
+            # Create a table with tokens as rows and annotators as columns
+            table = self.create_single_annotations_table(annotated_df)
+
+            # Accumulate the annotations in the accumulated_coefficients_table
+            accumulated_table = pd.concat([accumulated_table, table], axis=0)
+        return accumulated_table
+
+    def get_agreements_lists(self, df):
+        df = df.fillna('None')
+        # Count the occurrences of each label for each token
+        label_counts = df.apply(lambda x: x.value_counts(), axis=1)
+
+        # Calculate the total number of annotators
+        total_annotators = len(df.columns)
+
+        # Initialize dictionaries to store the counts of partial and full agreements for each label
+        partial_label_agreements = {}
+        full_label_agreements = {}
+
+        # Iterate through each token and its label counts
+        for _, counts in label_counts.iterrows():
+            # Check if there is a full agreement for any label
+            full_agreement = any(count == total_annotators for count in counts)
+
+            # If there is no full agreement, then it's a partial agreement
+            if not full_agreement:
+                for label, count in counts.items():
+                    # get the highest count and label in counts.items()
+                    if count == counts.max():
+                        if not pd.isna(label) :
+                            partial_label_agreements[label] = partial_label_agreements.get(label, 0) + 1
+            else:
+                # If there is a full agreement, increment the count for the fully agreed label
+                majority_label = counts.idxmax()
+                full_label_agreements[majority_label] = full_label_agreements.get(majority_label, 0) + 1
+
+        all_keys = set(full_label_agreements.keys()) | set(partial_label_agreements.keys())
+        all_agreements = [[key, full_label_agreements.get(key, 0), partial_label_agreements.get(key, 0)] for key in all_keys]
+
+        return all_agreements
+
+    def list_To_String(self, List: list) -> str:
+        """
+            Converts a list into a string 
+
+            Parameters:
+                List :
+                    The object of type list to convert to string
+                    
+            Returns:
+                The converted object from list into type string
+                
+        """    
+        str1 = " "    
+        return (str1.join(List))    
+
+
+
+
+                
+                
+                
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
